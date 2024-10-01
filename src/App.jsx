5e8e487159c2e26e36a2ea6c683dc66b806f9d25
mukimuddin <mdmukimuddin.bd@gmail.com>
2024-10-01 21:51:19 +0600

    Navbar not shows

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './App.css'
+import LayoutOne from './Layouts/LayoutOne'
 
 function App() {
-
+        const myRoutes = createBrowserRouter(createRoutesFromElements(
+          <Route>
+            <Route path='/' element={<LayoutOne />} >
+            
+            </Route>
+          </Route>
+        ))
   return (
     <>
+    <RouterProvider router={myRoutes} />
+
+
     </>
   )
 }
